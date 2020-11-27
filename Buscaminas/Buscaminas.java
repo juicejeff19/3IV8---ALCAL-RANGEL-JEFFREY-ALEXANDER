fdc144091f3c19e55b83aeab7b8b2c27e2d0df04
Jeffrey Alcalá <72107227+juicejeff19@users.noreply.github.com>
2020-11-27 05:28:44 -0600

    Avance 2, Dificultades

--- Buscaminas/Buscaminas.java
+++ Buscaminas/Buscaminas.java
@@ -131,12 +131,294 @@ public class Buscaminas extends Heren{
       }while (intento>=tminas && intento>0 && tminas>0);	
  	   
        	if(tminas==0)
-       		System.out.println("		GANASTE!");
+       		yay();
        	
        	if(tminas>intento)
-       		System.out.println("		PERDISTE!    (Exceso de minas restantes "+tminas+")\n\n");
+       		efe();
        	
        	if(intento==0)
-       		System.out.println("		PERDISTE!");
- 	   }
+       		efe2();
+        }
+        //Creo metodos sencillos para disminuir la complejidad del metodo principal, ya que codeFactor me pone una mala calificación si es demasiado complejo D:
+        public void efe(){
+            System.out.println("Perdiste, hay mas minas que intentos, F");
+        }
+        public void efe2(){
+            System.out.println("Perdiste, te quedaste sin intentos :(");
+        }
+        public void yay(){
+            System.out.println("Felicidades, desactivaste todas las minas, ganaste :D");
+        }
+       
+       
+       
+       
+       
+       
+        public void juego1(){
+		
+            //se crea un array, de 20x20, el cual sera nuesto "Tablero", aunque no se representara graficamente
+            int tablero [][]= new int[20][20];
+            
+            double w=0;
+              double z=0;
+            
+            int ntminas=0;
+            int intento=75;
+            
+            int tminas=10;
+            
+            for(int j=0;j<20;j++)
+                for (int i=0;i<20;i++)
+                    tablero [j][i]=0;
+            
+            do  {  
+                     w=Math.random()*20;
+                    z=Math.random()*20;  
+                       w=(int)w;
+                       z=(int)z;
+                       if  (z!=0 && w!=0 && z!=20-1 && w!=20-1){
+                     tablero[(int)w][(int) z ]=1;
+                      ntminas++;
+                           }
+                   }while (ntminas<=10);
+           
+           
+            // for(int xx=0;xx<20;xx++)
+           //	for (int yy=0;yy<20;yy++)
+           //     if(tablero [xx][yy]==1)
+           //     System.out.println("Fila   "+(xx+1)+"     columna "+(yy+1)); 
+          
+            System.out.println("\n****************************************************************");
+            System.out.println("\nHola "+getGamertag());
+            System.out.println("\nSuerte "+getNombre());
+            System.out.println("\nTu ID es: "+getId());
+            System.out.println("\n****************************************************************");
+               System.out.println("  Ingresa una coordenada, seleccionando dos numeros del 1 al 20");
+               do {
+                   int x=0,y=0;
+                   int nminas = 0;
+                   
+                   do{
+                        System.out.println("\n-******************************************************");
+                        System.out.println("\n	Intento :"+intento+" , Ingrese una coordenada? \n");
+                        System.out.print("		Fila:  ");
+                        x=entrada.nextInt();
+                        System.out.println();
+                        System.out.print("		Columna:  ");
+                        y=entrada.nextInt();
+                        
+                        if(x==0 || y==0 || x>20 || y>20)
+                            System.out.println("\n	==>Ojo a la comedia<== \n");
+                   }while (x==0 || y==0 || x>20 || y>20);
+                   
+                   x=x-1;
+                   y=y-1;
+                   
+                   if(tablero [x][y]==0){
+                       
+                       if (x==0&&y==0){
+                           if(x+1>=0 && x+1<20 && y<20)
+                               nminas=nminas+tablero [x+1][y];
+                           if(x+1>=0 && x+1<20 && y+1<20 && y+1>=0)
+                               nminas=nminas+tablero [x+1][y+1];
+                           if(x>=0 && x<20 && y+1<20 && y+1>= 0)
+                               nminas=nminas+tablero [x][y+1];
+                       }
+                       else {
+                           if(y==0){
+                               if(x+1>=0 && x+1<20 && y<20)
+                                   nminas=nminas+tablero [x+1][y];
+                               if(x-1>=0 && x-1<20 && y<20)
+                                   nminas=nminas+tablero [x-1][y];
+                               if(x-1>=0 && x-1<20 && y+1<20 && y+1>=0)
+                                   nminas=nminas+tablero [x-1][y+1];
+                               if(x>=0 && x<20 && y+1<20 && y+1>= 0)
+                                   nminas=nminas+tablero [x][y+1];
+                               if(x+1>=0 && x+1<20 && y+1<20 && y+1>=0)
+                                   nminas=nminas+tablero [x+1][y+1];
+                               
+                           }
+                           
+                           else{
+                           
+                               if(x-1>=0 && x-1<20 && y-1<20)
+                                   nminas=nminas+tablero [x-1][y-1];
+                               if(x>=0 && x<20 && y-1<20)
+                                   nminas=nminas+tablero [x][y-1];
+                               if(x+1>=0 && x+1<20 && y-1<20)
+                                   nminas=nminas+tablero [x+1][y-1];
+                               if(x+1>=0 && x+1<20 && y<20)
+                                   nminas=nminas+tablero [x+1][y];
+                               if(x-1>=0 && x-1<20 && y<20)
+                                   nminas=nminas+tablero [x-1][y];
+                               if(x-1>=0 && x-1<20 && y+1<20 && y+1>=0)
+                                   nminas=nminas+tablero [x-1][y+1];
+                               if(x>=0 && x<20 && y+1<20 && y+1>= 0)
+                                   nminas=nminas+tablero [x][y+1];
+                               if(x+1>=0 && x+1<20 && y+1<20 && y+1>=0)
+                                   nminas=nminas+tablero [x+1][y+1];
+                               }
+                        }
+                        System.out.print("\n	Mina no encontrada. ");
+                        
+                        if(intento>tminas)
+                             System.out.print("	Alrededor hay: "+ nminas);
+                        System.out.println("\n	Intentos restantes: "+(intento-1));
+                   }
+                   else
+                       {
+                           tminas = tminas-1;
+                           System.out.println("\n	Acertaste!, Mina Eliminada Restan "+tminas+" Minas");
+                           System.out.println("\n	Intentos restantes: "+(intento-1));
+                       }
+               
+              intento=intento-1;
+          }while (intento>=tminas && intento>0 && tminas>0);	
+            
+               if(tminas==0)
+                   yay();
+               
+               if(tminas>intento)
+                   efe();
+               
+               if(intento==0)
+                   efe2();
+            }
+            
+            
+            
+            
+            
+            public void juego2(){
+		
+                //se crea un array, de 30x30, el cual sera nuesto "Tablero", aunque no se representara graficamente
+                int tablero [][]= new int[30][30];
+                
+                double w=0;
+                  double z=0;
+                
+                int ntminas=0;
+                int intento=70;
+                
+                int tminas=10;
+                
+                for(int j=0;j<20;j++)
+                    for (int i=0;i<20;i++)
+                        tablero [j][i]=0;
+                
+                do  {  
+                         w=Math.random()*30;
+                        z=Math.random()*30;  
+                           w=(int)w;
+                           z=(int)z;
+                           if  (z!=0 && w!=0 && z!=30-1 && w!=30-1){
+                         tablero[(int)w][(int) z ]=1;
+                          ntminas++;
+                               }
+                       }while (ntminas<=10);
+               
+               
+                // for(int xx=0;xx<20;xx++)
+               //	for (int yy=0;yy<20;yy++)
+               //     if(tablero [xx][yy]==1)
+               //     System.out.println("Fila   "+(xx+1)+"     columna "+(yy+1)); 
+              
+                System.out.println("\n****************************************************************");
+                System.out.println("\nHola "+getGamertag());
+                System.out.println("\nSuerte "+getNombre());
+                System.out.println("\nTu ID es: "+getId());
+                System.out.println("\n****************************************************************");
+                   System.out.println("  Ingresa una coordenada, seleccionando dos numeros del 1 al 30");
+                   do {
+                       int x=0,y=0;
+                       int nminas = 0;
+                       
+                       do{
+                            System.out.println("\n-******************************************************");
+                            System.out.println("\n	Intento :"+intento+" , Ingrese una coordenada? \n");
+                            System.out.print("		Fila:  ");
+                            x=entrada.nextInt();
+                            System.out.println();
+                            System.out.print("		Columna:  ");
+                            y=entrada.nextInt();
+                            
+                            if(x==0 || y==0 || x>30 || y>30)
+                                System.out.println("\n	==>Ojo a la comedia<== \n");
+                       }while (x==0 || y==0 || x>30 || y>30);
+                       
+                       x=x-1;
+                       y=y-1;
+                       
+                       if(tablero [x][y]==0){
+                           
+                           if (x==0&&y==0){
+                               if(x+1>=0 && x+1<30 && y<30)
+                                   nminas=nminas+tablero [x+1][y];
+                               if(x+1>=0 && x+1<30 && y+1<30 && y+1>=0)
+                                   nminas=nminas+tablero [x+1][y+1];
+                               if(x>=0 && x<30 && y+1<30 && y+1>= 0)
+                                   nminas=nminas+tablero [x][y+1];
+                           }
+                           else {
+                               if(y==0){
+                                   if(x+1>=0 && x+1<30 && y<30)
+                                       nminas=nminas+tablero [x+1][y];
+                                   if(x-1>=0 && x-1<30 && y<30)
+                                       nminas=nminas+tablero [x-1][y];
+                                   if(x-1>=0 && x-1<30 && y+1<30 && y+1>=0)
+                                       nminas=nminas+tablero [x-1][y+1];
+                                   if(x>=0 && x<30 && y+1<30 && y+1>= 0)
+                                       nminas=nminas+tablero [x][y+1];
+                                   if(x+1>=0 && x+1<30 && y+1<30 && y+1>=0)
+                                       nminas=nminas+tablero [x+1][y+1];
+                                   
+                               }
+                               
+                               else{
+                               
+                                   if(x-1>=0 && x-1<30 && y-1<30)
+                                       nminas=nminas+tablero [x-1][y-1];
+                                   if(x>=0 && x<30 && y-1<30)
+                                       nminas=nminas+tablero [x][y-1];
+                                   if(x+1>=0 && x+1<30 && y-1<30)
+                                       nminas=nminas+tablero [x+1][y-1];
+                                   if(x+1>=0 && x+1<30 && y<30)
+                                       nminas=nminas+tablero [x+1][y];
+                                   if(x-1>=0 && x-1<30 && y<30)
+                                       nminas=nminas+tablero [x-1][y];
+                                   if(x-1>=0 && x-1<30 && y+1<30 && y+1>=0)
+                                       nminas=nminas+tablero [x-1][y+1];
+                                   if(x>=0 && x<30 && y+1<30 && y+1>= 0)
+                                       nminas=nminas+tablero [x][y+1];
+                                   if(x+1>=0 && x+1<30 && y+1<30 && y+1>=0)
+                                       nminas=nminas+tablero [x+1][y+1];
+                                   }
+                            }
+                            System.out.print("\n	Mina no encontrada. ");
+                            
+                            if(intento>tminas)
+                                 System.out.print("	Alrededor hay: "+ nminas);
+                            System.out.println("\n	Intentos restantes: "+(intento-1));
+                       }
+                       else
+                           {
+                               tminas = tminas-1;
+                               System.out.println("\n	Acertaste!, Mina Eliminada Restan "+tminas+" Minas");
+                               System.out.println("\n	Intentos restantes: "+(intento-1));
+                           }
+                   
+                  intento=intento-1;
+              }while (intento>=tminas && intento>0 && tminas>0);	
+                
+                   if(tminas==0)
+                       yay();
+                   
+                   if(tminas>intento)
+                       efe();
+                   
+                   if(intento==0)
+                       efe2();
+                }
+
 }
--- Buscaminas/Datos.java
+++ Buscaminas/Datos.java
@@ -3,7 +3,7 @@ public class Datos{
     //declaro el Scanner
     Scanner entrada = new Scanner(System.in);
     //no estoy seguro de que poner como herencia en un juego, asi que seran los datos del jugador
-    //Declaro las variables privadas
+    //Declaro las variables
      String nombre, gamertag;
      int id, decision;
     //declaro el metodo para recopilar los datos
@@ -18,18 +18,19 @@ public class Datos{
         System.out.println("Ese es un gran gamertag, ahora, diseña tu id, recuerda que tiene que ser un numero entero");
         id = entrada.nextInt();
         System.out.println("Instrucciones:\nTienes un numero limitado de intentos, tendras que encontrar las minas para asi desactivarlas\n¿Quien no queria ser un Desactivador de minas de niño?");
-        System.out.println("Pulsa 1 cuando estes listo para empezar");
+        System.out.println("Selecciona la dificultad\n1. Facil\n2. Intermedia\n3 .Dificil\4. Imposible");
         decision = entrada.nextInt();
-        if (decision==1){
-        System.out.println("Estamos listos para empezar :)");
         //creo el objeto que me llevara al juego, mandandolo con mis variables recopiladas
         Buscaminas dear = new Buscaminas(nombre, gamertag, id);
+        if (decision==1){
+        System.out.println("Estamos listos para empezar :), La dificultad que has elegido es Facil");
         //vamos para el juego
         dear.juego();
-        }else if(decision==69){
-            System.out.println("Ja,ja,ja,ja,ja");
+        }else if(decision==2){
+            System.out.println("Estamos listos para empezar :), La dificultad que has elegido es Intermedia");
+
         }else{
-            System.out.println("En realidad no tenias opcion xd supongo que no estas preparado");
+
         }
 
 
