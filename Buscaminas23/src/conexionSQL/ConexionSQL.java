5e40ce7cf83f91bebdafff34bd5756e94c8daa0b
Jeffrey Alcalá <72107227+juicejeff19@users.noreply.github.com>
2021-01-03 01:02:28 -0600

    tuve fe

---
+++ Buscaminas23/src/conexionSQL/ConexionSQL.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package conexionSQL;
+
+import com.mysql.jdbc.Connection;
+import java.sql.DriverManager;
+import javax.swing.*;
+
+/**
+ *
+ * @author juice_pjuorme
+ */
+public class ConexionSQL {
+    
+    Connection conectar = null;
+    
+    public Connection conexion(){
+        
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            conectar = (Connection) DriverManager.getConnection("jdbc:mysql://localhost:3306/buscaminas","root", "");
+            JOptionPane.showMessageDialog(null,"Conexion Exitosa");
+            
+        }catch(Exception e){
+            JOptionPane.showMessageDialog(null,"Error: "+e.getMessage());
+        }
+        return conectar;
+    }
+    
+}
